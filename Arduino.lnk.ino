21c7351e0a802bef94417a05befb288d6956ef31
CHMachine <cockheromachine@gmail.com>
2020-02-16 16:19:49 +0100

    first commit

---
+++ Arduino.lnk.ino
@@ -0,0 +1,92 @@
+/*
+  COCK HERO MACHINE Pro serial connection V.0.9
+  http://cockheromachine.blogspot.it/
+  cockheromachine@gmail.com
+  
+  This program is expected to receive over serial an 8bit integer value between the string 'V' and any other string(e.g. V100S) to vary the pulse width accordingly on the analog pin. 
+  The pin PWM goes to 0 after 200ms if no integers are sent.
+  
+*/
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#include <SoftwareSerial.h>
+int Value = 0;
+int pinPWM = 5;
+int rxPin = 2;
+int txPin = 3;
+int timelimit = 200;
+long unsigned timer = 0;
+SoftwareSerial mySerial(rxPin, txPin); //rx,tx
+
+void setup() {
+  pinMode(pinPWM, OUTPUT);// initialize the digital pin as an output.
+  analogWrite(pinPWM, 0);
+  cbi(ADCSRA, ADEN); // Switch Analog to Digital converter OFF
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  //pinMode(3, INPUT_PULLUP); 
+  //pinMode(4, INPUT_PULLUP); //pulling up unused pin, da non fare se si usa xtal
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+  if (mySerial.available() > 0) {
+
+    if (mySerial.peek() == 'V') {      //The 'V' character preced the PWM value for the PWM pin
+      mySerial.read();                 //remove the first character(V)
+      Value = mySerial.parseInt();     //read the value
+      mySerial.read();                //remove the last character(S)
+      analogWrite(pinPWM, Value);       // set the state of the PIN
+      timer = millis();              //reset the timer
+    }
+
+
+    else if (mySerial.peek() == 'T') {      //the character 'T' is used to check if the connection is established
+      mySerial.read();                 //clean the serial buffer
+      mySerial.print("connOK");         //send a string
+                
+    }
+    
+    else
+      mySerial.read();                 //clear the serial buffer
+
+
+  }
+
+  else if (Serial.available() > 0) {
+
+
+
+    if (Serial.peek() == 'V') {      //check for the character V
+      Serial.read();                 //remove the character V from buffer
+      Value = Serial.parseInt();     //read the integer value
+      Serial.read();                 //remove the last character(S)
+      analogWrite(pinPWM, Value);       // set the state of the PIN
+      timer = millis();              //reset the timer
+    }
+
+    //check if the connection is estabilished:
+    else if (Serial.peek() == 'T') {      //check for the character T
+      Serial.read();                      //clean the serial buffer
+      Serial.print("connOK");           //send a string
+      
+         
+    }
+    else
+      Serial.read();                 //clear the serial buffer from other characters
+
+
+  }
+  
+
+
+  if (millis() - timer >= timelimit) { //turn off the pin after some time if no character 'K' is received
+    analogWrite(pinPWM, 0);
+  }
+
+}
